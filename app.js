b5c0436e371636b969bc3f2710e19803fe55ffa6
Aarun1349 <arunmahendra90@gmail.com>
2023-02-06 16:10:24 +0530

    Initial Commit: Setup app for further development and add nodemon mongoose dotenv and express

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express();
+
+
+//setting up config.env valiable
+dotenv.config({path:'./config/config.env'})
+const port = process.env.PORT || 8000;
+
+
+//routes
+app.get('/',(req,res)=>{
+    res.send(200).json({success:"Success"})
+})
+
+app.listen(port,()=>{
+    console.log(`App is running on port ${port}`)
+})
+
