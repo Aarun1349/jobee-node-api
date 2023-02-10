e34b85808bb425b9b9f41cb8617c32df659ca7ca
Aarun1349 <arunmahendra90@gmail.com>
2023-02-10 09:25:17 +0530

    Add User Model

---
+++ models/users.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Please enter your name']
+    },
+    email:{
+        type:String,
+        required:[true,'Please enter your email address'],
+        unique:true,
+        validate:[validator.isEmail,'Please enter valid email address']
+    },
+    role:{
+        type:String,
+        enum:{
+            values:['user','employer'],
+            message:'Please select correct role'
+        },
+        default:'user'
+    },
+    password:{
+        type:String,
+        required:[true,'Please provide password'],
+        minlength:[8,'Your password must be at least 8 characters length'],
+        select:false
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    resetPasswordToken:String,
+    resetPasswordExipre:Date
+})
+
+module.exports = mongoose.model('User',userSchema);
